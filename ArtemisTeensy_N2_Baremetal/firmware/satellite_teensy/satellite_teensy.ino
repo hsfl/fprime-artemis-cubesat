66f19e155f143689711f28b615c1b3e097e1e4b9
Dennis Sarsozo <dsarsozo@hawaii.edu>
2026-02-05 13:49:38 -1000

    turn on teensy led on and raspberry pi on on boot

--- ArtemisTeensy_N2_Baremetal/firmware/satellite_teensy/satellite_teensy.ino
+++ ArtemisTeensy_N2_Baremetal/firmware/satellite_teensy/satellite_teensy.ino
@@ -9,6 +9,8 @@ static constexpr int RADIO_CS = 38;
 static constexpr int RADIO_INT = 40;
 static constexpr uint8_t RADIO_RX_ON_PIN = 30;
 static constexpr uint8_t RADIO_TX_ON_PIN = 31;
+static constexpr uint8_t RPI_ENABLE_PIN = 36;
+static constexpr uint8_t TEENSY_LED_PIN = 13;
 
 static constexpr uint32_t UART_BAUD = 115200;
 
@@ -18,6 +20,14 @@ RelayUartRf g_relay(Serial2, g_rfDriver, g_linkCounters);
 
 void setup() {
   Serial.begin(115200);
+
+  // Match EPSCOR payload baseline: assert Pi power-enable at boot.
+  pinMode(RPI_ENABLE_PIN, OUTPUT);
+  digitalWrite(RPI_ENABLE_PIN, HIGH);
+  pinMode(TEENSY_LED_PIN, OUTPUT);
+  digitalWrite(TEENSY_LED_PIN, HIGH);
+  Serial.println("[ArtemisTeensy] RPI power enable asserted (pin 36 HIGH)");
+  Serial.println("[ArtemisTeensy] LED asserted (pin 13 HIGH)");
 
   const bool radioOk = g_rfDriver.begin();
   g_relay.begin(UART_BAUD);
